c1c0be31b3006732fc9db751651a7ef81e310691
Rhiannon Moore <rhiannonmoore10@yahoo.com>
2023-03-03 15:10:54 -0700

    Adds functions with parameters

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Follow along with the examples here
+function doNothing() {}
+function sayHello() {
+    console.log("Hello!");
+  }
+sayHello();
+
+function sayHelloToGuadalupe() {
+    console.log("Hello, Guadalupe!");
+  }
+  function sayHelloToLiz() {
+    console.log("Hello, Liz!");
+  }
+  
+  function sayHelloToSamip() {
+    console.log("Hello, Samip!");
+  }
+  sayHelloToGuadalupe();
+  sayHelloToLiz();
+  sayHelloToSamip();
+
+  function say(greeting, firstName) {
+    console.log(`${greeting}, ${firstName}!`);
+  }
+  say("Goodbye", "Julio");
+  
+  
+  function say(greeting, firstName) {
+    console.log("I was called!");
+    return `${greeting}, ${firstName}!`;
+  }
+  
+  console.log(say("Howdy", "partner"));
+  
+  
